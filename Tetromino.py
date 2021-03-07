16a3476d6ea9f902568568309192e8a8817156c6
DuongHieu <duonghieu0712z@gmail.com>
2021-03-07 19:01:56 +0700

    Add Tetromino.py

---
+++ Tetromino.py
@@ -0,0 +1,13 @@
+import pygame
+from header import *
+
+
+class Tetromino:
+    def __init__(self) -> None:
+        pass
+
+    def randomizeBlock(self) -> None:
+        pass
+
+    def draw(self, surface) -> None:
+        pass
