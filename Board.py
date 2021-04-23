7505fb71fbfb74f48043da1da0bee30d85de48de
DuongHieu <duonghieu0712z@gmail.com>
2021-04-23 08:33:51 +0700

    Update Board.py

--- Board.py
+++ Board.py
@@ -7,27 +7,32 @@ class Board:
         self.pos = pygame.Vector2(BOARD_POS)
         self.width = BOARD_WIDTH
         self.height = BOARD_HEIGHT
-        self.row = 20
-        self.col = 10
+        self.row = ROW
+        self.col = COL
+        self.matrix = [[-1] * self.col] * self.row
         self.size = BLOCK_SIZE
         self.surface = pygame.Surface((self.width, self.height))
 
-    def draw(self, surface: pygame.Surface) -> None:
+    def draw(self, surface: pygame.Surface, block) -> None:
         self.surface.fill(WHITE)
 
-        y = 0
         for row in range(self.row):
-            x = 0
             for col in range(self.col):
-                if (row + col) % 2:
-                    color = (232, 232, 232)
+                if self.matrix[row][col] == -1:
+                    if (row + col) % 2:
+                        color = (232, 232, 232)
+                    else:
+                        color = (248, 248, 248)
                 else:
-                    color = (248, 248, 248)
-                rect = pygame.Rect(x, y, self.size, self.size)
-                pygame.draw.rect(self.surface, color, rect)
-                x += self.size
-            y += self.size
+                    color = COLORS[self.matrix[row][col]]
 
+                x = self.size * col
+                y = self.size * row
+                rect = pygame.Rect(x, y, self.size, self.size)
+
+                pygame.draw.rect(self.surface, color, rect)
+
+        block.draw(self.surface)
         surface.blit(self.surface, self.pos)
         frame = pygame.Rect(self.pos.x - 1, self.pos.y - 1,
                             self.width + 2, self.height + 2)
